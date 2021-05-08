ed701dd141a35658c86eb3577c2d34962f0ac133
ydean892 <61839768+ydean892@users.noreply.github.com>
2021-05-08 09:40:12 +0530

    first get route

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name:mark},
+    {name:jill}
+]
+
+app.get('/users',function(req,res){
+    res.json({
+        success:true,
+        message:'sucessfully got the data',
+        users:mockUserData
+    })
+})
 
 app.listen(8000,function(){
     console.log('server is running');
